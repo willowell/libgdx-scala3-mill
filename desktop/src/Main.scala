20a4ee12b910269f8ead93f6260d05d0cf444c5c
William Howell <wlm.howell@gmail.com>
2022-06-27 12:03:52 -0400

    some tabs snuck in

--- desktop/src/Main.scala
+++ desktop/src/Main.scala
@@ -1,4 +1,4 @@
-package core.test
+package desktop
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
@@ -6,7 +6,7 @@ import core.MyGdxGame
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 @main def runGame() =
-	val config = Lwjgl3ApplicationConfiguration()
-	config.setForegroundFPS(60)
-	config.setTitle("My GDX Game")
-	Lwjgl3Application(MyGdxGame(), config)
+  val config = Lwjgl3ApplicationConfiguration()
+  config.setForegroundFPS(60)
+  config.setTitle("My GDX Game")
+  Lwjgl3Application(MyGdxGame(), config)
